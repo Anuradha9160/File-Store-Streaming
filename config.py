3cbc222d7dc9b96b5fc0f47e107b389fdc69df7f
KingCobra91 <117145604+KingCobra91@users.noreply.github.com>
2024-03-06 13:31:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -58,6 +58,10 @@ LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1001589444483"))
 FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '-1001984125625')).split()]
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001988835535"))
 
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
 #Force sub message Information
 FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel</b>")
 
